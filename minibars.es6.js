a53bc828d48ae4d73ad0a51c8cc10fce0f061ac5
Udo Schroeter <udo.schroeter@gmail.com>
2015-09-01 15:20:39 +0200

    separate normal and es6 version, more testing

--- minibars.es6.js
+++ minibars.es6.js
@@ -1,22 +1,24 @@
+'use strict';
 // author: udo.schroeter@gmail.com
+// caution: right now this only works on Chrome!
 
-Minibars = {
+const Minibars = {
   
   scope : 'data',
   stack : [],
   
   tokenize : function(text) {
-    var tokens = [];
-    for(var i = 0; i < text.length; i++) {
-      var tkPos = text.indexOf('{{', i);
+    const tokens = [];
+    for(let i = 0; i < text.length; i++) {
+      const tkPos = text.indexOf('{{', i);
       if(tkPos === -1) {
         tokens.push({ type : 'text', val : text.slice(i) });
         i = text.length;
       } 
       else {
-        var rawField = text.substr(tkPos, 3) == '{{{';
-        var closeBy = '}}' + (rawField ? '}' : '');
-        var tkEnd = text.indexOf(closeBy, tkPos);
+        const rawField = text.substr(tkPos, 3) == '{{{';
+        const closeBy = '}}' + (rawField ? '}' : '');
+        const tkEnd = text.indexOf(closeBy, tkPos);
         if(tkEnd === -1) {
           tokens.push({ type : 'text', val : text.slice(i) });
           i = text.length;
@@ -24,24 +26,24 @@ Minibars = {
           if(i < tkPos)
             tokens.push({ type : 'text', val : text.slice(i, tkPos) });
             
-          var token = { 
+          const token = { 
             type : 'field', 
             raw : rawField, 
             val : text.slice(tkPos+closeBy.length, tkEnd), 
             };
             
-          var delimHS = token.val.indexOf(' ');
+          const delimHS = token.val.indexOf(' ');
           if(delimHS !== -1) {
             token.params = token.val.slice(delimHS+1);
             token.val = token.val.slice(0, delimHS);
-            var delimMap = token.params.indexOf('|');
+            const delimMap = token.params.indexOf('|');
             if(delimMap !== -1) {
               token.mapTo = token.params.slice(delimMap+1).trim().split(' ');
               token.params = token.params.slice(0, delimMap);
             }
           }
 
-          var prefix = token.val.substr(0, 1);
+          const prefix = token.val.substr(0, 1);
           if(prefix === '#') {
             token.block = 'start';
             token.val = token.val.slice(1);
@@ -81,8 +83,8 @@ Minibars = {
       if(label[0] == '"' && label[label.length-1] == '"') return(JSON.stringify(label.slice(1, -1)));
       if(label == 'this') return(Minibars.scope);
       if(label[0] == '@') return(label.slice(1));
-      var scope = Minibars.scope;
-      var upCnt = 0;
+      let scope = Minibars.scope;
+      let upCnt = 0;
       while(label[0] == '.') {
         // access parent scopes with ..
         if(upCnt > 0 && Minibars.stack.length >= upCnt) {
@@ -91,8 +93,8 @@ Minibars = {
         upCnt++;
         label = label.slice(1);
       }
-      var levels = label.split('.');
-      var acc = '';
+      const levels = label.split('.');
+      let acc = '';
       levels.forEach(function(fn) {
         if(fn[0] == '/') fn = fn.slice(1);
         acc += '['+JSON.stringify(fn)+']';
@@ -144,14 +146,14 @@ Minibars = {
     },
     
     each_start : function(token) {
-      var container = Minibars.utils.field(token.params.trim());
-      var iterVN = Minibars.utils.getName();
-      var scp = Minibars.utils.pushScope();
+      const container = Minibars.utils.field(token.params.trim());
+      const iterVN = Minibars.utils.getName();
+      const scp = Minibars.utils.pushScope();
       return(
         'if('+container+'&&'+container+'.length>0) '+
-        'for(var '+iterVN+'=0;'+iterVN+'<'+container+'.length;'+iterVN+'++) {'+
-        'var index='+iterVN+';'+
-        'var '+scp+'='+container+'['+iterVN+'];');
+        'for(let '+iterVN+'=0;'+iterVN+'<'+container+'.length;'+iterVN+'++) {'+
+        'const index='+iterVN+';'+
+        'const '+scp+'='+container+'['+iterVN+'];');
     },
     
     each_end : function(token) {
@@ -161,12 +163,12 @@ Minibars = {
     
     local_mapping : function(token, tm) {
       if(token.mapTo && token.mapTo.length > 0) {
-        var mapCode = '';
-        for(var i = 0; i < token.mapTo.length; i++) {
-          var alias = token.mapTo[i];
-          var v = tm[i];
+        let mapCode = '';
+        for(let i = 0; i < token.mapTo.length; i++) {
+          const alias = token.mapTo[i];
+          const v = tm[i];
           if(alias && v)
-            mapCode += 'var '+alias+' = '+v+';'; 
+            mapCode += 'const '+alias+' = '+v+';'; 
         }
         return(mapCode);
       } else {
@@ -175,17 +177,17 @@ Minibars = {
     },
     
     properties_start : function(token) {
-      var container = Minibars.utils.field(token.params.trim());
-      var iterVN = Minibars.utils.getName();
-      var keyVN = Minibars.utils.getName();
-      var scp = Minibars.utils.pushScope();
+      const container = Minibars.utils.field(token.params.trim());
+      const iterVN = Minibars.utils.getName();
+      const keyVN = Minibars.utils.getName();
+      const scp = Minibars.utils.pushScope();
       return(
-        'var '+keyVN+'=Object.keys('+container+');'+
+        'let '+keyVN+'=Object.keys('+container+');'+
         'if('+container+'&&'+keyVN+'.length>0) '+
-        'for(var '+iterVN+'=0; '+iterVN+'<'+keyVN+'.length;'+iterVN+'++) {'+
-        'var index='+iterVN+';'+
-        'var key='+keyVN+'['+iterVN+'];'+
-        'var '+scp+'='+container+'[key];'+
+        'for(let '+iterVN+'=0; '+iterVN+'<'+keyVN+'.length;'+iterVN+'++) {'+
+        'const index='+iterVN+';'+
+        'const key='+keyVN+'['+iterVN+'];'+
+        'const '+scp+'='+container+'[key];'+
         Minibars.gen.local_mapping(token, [scp, 'key']));
     },
     
@@ -195,11 +197,11 @@ Minibars = {
     },
     
     with_start : function(token) {
-      var container = Minibars.utils.field(token.params.trim());
-      var scp = Minibars.utils.pushScope();
+      const container = Minibars.utils.field(token.params.trim());
+      const scp = Minibars.utils.pushScope();
       return(
         'if('+container+') { '+
-        'var '+scp+'='+container+';'+
+        'const '+scp+'='+container+';'+
         Minibars.gen.local_mapping(token, [scp]));
     },
     
@@ -222,7 +224,7 @@ Minibars = {
     
     _field_f : function(token) {
       if(token.block == 'start' || token.block == 'end') {
-        var genKey = token.val+'_'+token.block;
+        const genKey = token.val+'_'+token.block;
         if(Minibars.gen[genKey])
           return(Minibars.gen[genKey](token));
         else {
@@ -247,10 +249,10 @@ Minibars = {
   },
   
   tokensToCode : function(tokens) {
-    var code = [];
+    const code = [];
     
     tokens.forEach(function(t) {
-      var handler = Minibars.gen['_'+t.type+'_f'];
+      const handler = Minibars.gen['_'+t.type+'_f'];
       if(typeof handler != 'function')
         console.log('unrecognized field type', t.type);
       else
@@ -265,16 +267,16 @@ Minibars = {
     Minibars.opt = opt;
     Minibars.stack = [];
     Minibars.scope = 'data';
-    var tokens = Minibars.tokenize(text.trim());
-    var code = Minibars.tokensToCode(tokens);
+    const tokens = Minibars.tokenize(text.trim());
+    const code = Minibars.tokensToCode(tokens);
     //console.log('code', code);
     if(Minibars.stack.length != 0) {
-      var e = function() { return('template error: un-closed blocks'); };
+      const e = function() { return('template error: un-closed blocks'); };
       e.error = e();
       return(e);      
     } else {
       // straight-up string concat seems to be the fastest option
-      return(eval('(function(data) { "use strict"; var output = ""; '+code+' return(output); })'));
+      return(eval('(function(data) { "use strict"; let output = ""; '+code+' return(output); })'));
     }
   },
   
